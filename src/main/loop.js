77ec6bd2d4cd944eb86ae8cb9779e87763d6374c
jsmeow <jan@cyborg.ninja>
2019-08-22 14:47:29 -0400

    experimenting with loop optimization

--- src/main/loop.js
+++ src/main/loop.js
@@ -7,11 +7,11 @@ function timestamp() {
 }
 
 // The previous frame datetime timestamp
-let last = timestamp();
+const last = timestamp();
 
 // The difference between the previous frame datetime timestamp and the current
 // Frame datetime timestamp.
-let delta = 0;
+const delta = 0;
 
 // Depending on the collision detection scheme, fast moving objects can pass
 // Through small objects, this can be mitigated if our fixed step is set
@@ -21,34 +21,14 @@ const step = 1 / fps;
 // An update loop frame
 // Actions taken on an update loop frame depend on the application state.
 function frame() {
-  // The current frame datetime timestamp
-  const now = timestamp();
-
-  // Resolve the delta
-  // One additional note is that requestAnimationFrame might pause if our
-  // Browser loses focus, resulting in a very, very large dt after it resumes.
-  // We can workaround this by limiting the delta to one second
-  delta = delta + Math.min(1, (now - last) / 1000);
-
-  while (delta >= step) {
-    delta = delta - step;
-    logic.update();
-  }
-
+  window.requestAnimationFrame(frame);
+  logic.update();
   logic.render();
-
-  last = now;
-
-  window.requestAnimationFrame(() => {
-    frame();
-  });
 }
 
 // Start the application loop
 function loop() {
-  window.requestAnimationFrame(() => {
-    frame();
-  });
+  window.requestAnimationFrame(frame);
 }
 
 module.exports = loop;
